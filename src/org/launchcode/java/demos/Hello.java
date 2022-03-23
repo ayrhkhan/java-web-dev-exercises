3673d004c381ddbe24d3025a4dfe0747fd0138f3
ayrhkhan <ayrhkhan@gmail.com>
2022-03-22 19:27:51 -0500

    A new move

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -5,6 +5,6 @@ package org.launchcode.java.demos;
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello, Worlds! ");
+        System.out.println("Hello, World. I am moving.");
     }
 }
